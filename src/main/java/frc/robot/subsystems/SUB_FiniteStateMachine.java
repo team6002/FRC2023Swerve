7275bdb2c2435d69e8885914e182a3b528babac7
Justin Nguyen <justinnguyen4675@gmail.com>
2023-01-20 19:48:33 -0500

    added test button mappings

---
+++ src/main/java/frc/robot/subsystems/SUB_FiniteStateMachine.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+public class SUB_FiniteStateMachine {
+
+    public enum RobotState 
+    {
+        HOME,
+        
+    }
+
+    private RobotState m_currentState = RobotState.HOME;
+
+    public void setState(RobotState p_State) {
+        m_currentState = p_State;
+    }
+    
+    public RobotState getState() {
+        return m_currentState;
+    }
+    
+    public boolean isState(RobotState p_State) {
+        return (m_currentState == p_State);
+    }
+
+    public RobotState getCurrentState(){
+        return m_currentState;
+    }
+    
+}

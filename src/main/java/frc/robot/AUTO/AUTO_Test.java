a71741afbe22379173387e65cc8d5594ee4218d8
Justin Nguyen <justinnguyen4675@gmail.com>
2023-01-20 20:33:25 -0500

    created base autonomous code

---
+++ src/main/java/frc/robot/AUTO/AUTO_Test.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.AUTO;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class AUTO_Test extends SequentialCommandGroup {
+  AUTO_Trajectory m_trajectory;
+  public AUTO_Test(AUTO_Trajectory p_trajectory) {
+    m_trajectory = p_trajectory;
+
+    addCommands(
+      new SequentialCommandGroup(
+        m_trajectory.driveTrajectory(m_trajectory.testTrajectory)
+      )
+  );
+  }
+}
